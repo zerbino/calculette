6c2b7cb68cdc7182d540a3024834c2e3260736c7
zerbino <r2mart10@gmail.com>
2013-05-24 13:43:10 +0200

    commit mit mit

--- Calculatrice/src/tests/Test.java
+++ Calculatrice/src/tests/Test.java
@@ -1,6 +1,11 @@
 package tests;
 
+import java.io.IOException;
 import java.util.ArrayList;
+
+import lexer.UnexistingToken;
+import AST.NoInputException;
+import AST.UnexpectedType;
 
 import token.Num;
 import token.Value;
@@ -8,8 +13,46 @@ import token.Value;
 public class Test {
 
 	public static void main(String[] args) {
-		Value value = new Num(3);
-		System.out.println(value instanceof Num);
+
+		Test test = new Test();
+		try {
+			test.test28();
+		} catch (UnexistingToken e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnexpectedType e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoInputException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+	
+	public void test28() throws UnexistingToken, IOException, UnexpectedType, NoInputException {
+
+	    if (true) System.out.printf("%nRandoopTest0.test28");
+
+
+	    AST.Base var0 = new AST.Base();
+	    var0.parse();
+	    token.Float var3 = new token.Float((-1.0d));
+	    boolean var4 = parameters.ModeManager.rightType((token.Value)var3);
+	    // The following exception was thrown during execution.
+	    // This behavior will recorded for regression testing.
+	    try {
+	      var0.parse((token.Token)var3);
+	    } catch (AST.UnexpectedType e) {
+	      // Expected exception.
+	    	e.printStackTrace();
+	    }
+	    
+	    
+	    // Regression assertion (captures the current behavior of the code)
+
+	  }
 
 }
